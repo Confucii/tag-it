79caf10db096b65a15c19b78710ccb3f8cd5e743
Oleksii <lesha.avdeev@gmail.com>
2023-06-08 23:45:35 +0300

    Tweak styling

--- src/components/Leaderboard/Score.tsx
+++ src/components/Leaderboard/Score.tsx
@@ -1,11 +1,17 @@
 import "./styles/Score.css";
 
 function Score({ id, time, name }: { id: number; time: number; name: string }) {
+  const seconds = time % 60;
+  const minutes = (time - seconds) / 60;
+
+  const timeString =
+    String(minutes).padStart(2, "0") + " : " + String(seconds).padStart(2, "0");
+
   return (
     <tr className="Score">
       <td className="score-table-body-data">{id + 1}</td>
       <td className="score-table-body-data">{name}</td>
-      <td className="score-table-body-data">{time}</td>
+      <td className="score-table-body-data">{timeString}</td>
     </tr>
   );
 }
